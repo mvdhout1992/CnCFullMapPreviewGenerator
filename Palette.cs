f8c6d98c1dd745fe6f941ae319897b1d8466e425
iran <ma1kelvdh@gmail.com>
2013-09-16 11:49:37 +0200

    palette, SHP and template files loading and rendering working now

---
+++ Palette.cs
@@ -0,0 +1,89 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System;
+using System.Reflection;
+
+namespace CncFullMapPreviewGenerator
+{
+    public class Palette
+    {
+        uint[] colors;
+        public Color GetColor(int index)
+        {
+            return Color.FromArgb((int)colors[index]);
+        }
+
+        public void SetColor(int index, Color color)
+        {
+            colors[index] = (uint)color.ToArgb();
+        }
+
+        public void SetColor(int index, uint color)
+        {
+            colors[index] = (uint)color;
+        }
+
+        public uint[] Values
+        {
+            get { return colors; }
+        }
+
+        public Palette(Stream s, int[] remapShadow)
+        {
+            colors = new uint[256];
+
+            using (BinaryReader reader = new BinaryReader(s))
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    byte r = (byte)(reader.ReadByte() << 2);
+                    byte g = (byte)(reader.ReadByte() << 2);
+                    byte b = (byte)(reader.ReadByte() << 2);
+                    colors[i] = (uint)((255 << 24) | (r << 16) | (g << 8) | b);
+                }
+            }
+
+            colors[0] = 0; //convert black background to transparency
+            foreach (int i in remapShadow)
+                colors[i] = 140u << 24;
+        }
+
+        public Palette(Palette p, IPaletteRemap r)
+        {
+            colors = new uint[256];
+            for (int i = 0; i < 256; i++)
+                colors[i] = (uint)r.GetRemappedColor(Color.FromArgb((int)p.colors[i]), i).ToArgb();
+        }
+
+        public Palette(Palette p)
+        {
+            colors = (uint[])p.colors.Clone();
+        }
+
+        public ColorPalette AsSystemPalette()
+        {
+            ColorPalette pal;
+            using (var b = new Bitmap(1, 1, PixelFormat.Format8bppIndexed))
+                pal = b.Palette;
+
+            for (var i = 0; i < 256; i++)
+                pal.Entries[i] = GetColor(i);
+
+            // hack around a mono bug -- the palette flags get set wrong.
+            if (Platform.CurrentPlatform != PlatformType.Windows)
+                typeof(ColorPalette).GetField("flags",
+                    BindingFlags.Instance | BindingFlags.NonPublic).SetValue(pal, 1);
+
+            return pal;
+        }
+
+        public static Palette Load(string filename, int[] remap)
+        {
+            using (var s = File.OpenRead(filename))
+                return new Palette(s, remap);
+        }
+    }
+
+    public interface IPaletteRemap { Color GetRemappedColor(Color original, int index); }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace CncFullMapPreviewGenerator
 {
@@ -8,6 +9,15 @@ namespace CncFullMapPreviewGenerator
     {
         static void Main(string[] args)
         {
+            int[] ShadowIndex = { };
+            Palette Pal = Palette.Load("data/temperat.pal", ShadowIndex);
+            ShpReader SHP = ShpReader.Load("data/dome.shp");
+            Bitmap ShpBitmap = RenderUtils.RenderShp(SHP, Pal, 1);
+
+//            TemplateReader Template = TemplateReader.Load("data/bridge2h.tem", Pal);
+//            Bitmap ShpBitmap = RenderUtils.RenderTemplate(Template, Pal);
+
+            ShpBitmap.Save("derp.png");
         }
     }
 }
