0d68c7cc50a440b95a121cde04beda97a2aeac34
iran <ma1kelvdh@gmail.com>
2013-09-26 22:51:52 +0200

    add GPL licensing to OpenRA code and COPYING file

--- Platform.cs
+++ Platform.cs
@@ -2,6 +2,16 @@
 using System.Diagnostics;
 using System.IO;
 
+#region Copyright & License Information
+/*
+ * Copyright 2007-2012 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation. For more information,
+ * see COPYING.
+ */
+#endregion
+
 namespace CncFullMapPreviewGenerator
 {
     public enum PlatformType { Unknown, Windows, OSX, Linux }
